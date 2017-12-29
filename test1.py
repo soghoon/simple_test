65521506f9570c5d1ccc9aaab3bb042cbc59c60b
soghoon <antsonghoo@gist.ac.kr>
2017-12-29 16:24:33 +0900

    pids by online soghooon

--- test1.py
+++ test1.py
@@ -4,3 +4,8 @@ import scipy.constants as spc
 mc = spc.m_e * spc.c
 mc2 = spc.m_e * spc.c**2
 '''We do not need mc3 / if we need we can built it any time. please check. this'''
+
+"""soghoon, the online editor added this """"
+### Particle datas ###
+pids = [1,2,3,4,5]
+pgammas = [1.0,1.0,1.0,1.0,1.0]
