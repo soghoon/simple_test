04c3ed50bad24d31d4fd8550ecfc2541e08677de
hsong <antsonghoo@gist.ac.kr>
2017-12-29 16:19:14 +0900

    added particle informations

--- test1.py
+++ test1.py
@@ -4,3 +4,8 @@ import scipy.constants as spc
 mc = spc.m_e * spc.c
 mc2 = spc.m_e * spc.c**2
 '''We do not need mc3 / if we need we can built it any time. please check. this'''
+''' Ok.'''
+
+### Particle datas ###
+particle_IDs = [1,2,3,4,5]
+particle_gammas = [1.01,1.33,2.34,4.44,1.0]
